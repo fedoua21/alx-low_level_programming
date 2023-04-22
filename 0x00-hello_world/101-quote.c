05cd8c6352d93e55f38ce352988d148b7d80865f
fedoua <fedoualaasri91@gmail.com>
2023-04-21 19:44:23 -0700

    mycommit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - Entry point
- * Return: always 0 (success)
+ * main - Entry point.
+ * Return: always 1 (success).
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
