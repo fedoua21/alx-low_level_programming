30d8772fa2314a370cd46a5da850b2c26134214e
fedoua <fedoualaasri91@gmail.com>
2023-04-26 15:05:44 -0700

    mycommit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,14 +12,14 @@ void print_to_98(int n)
 	{
 		for (count = n; count < 98; count++)
 		{
-			printf("%d, ", n);
+			printf("%d, ", count);
 		}
 	}
 	else
 	{
 		for (count = n; count > 98; count--)
 		{
-			printf("%d, ", n);
+			printf("%d, ", count);
 		}
 	}
 	printf("98\n");
