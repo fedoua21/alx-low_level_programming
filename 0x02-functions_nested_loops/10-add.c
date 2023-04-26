69d39c9fb12a14cdb8a6a5d130e30869a1086669
fedoua <fedoualaasri91@gmail.com>
2023-04-26 14:26:38 -0700

    mycommit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *add - compute sum of tow menbers
+ *@a: first unput
+ *@b: second input
+ *Return: the sum of a and b
+*/
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
