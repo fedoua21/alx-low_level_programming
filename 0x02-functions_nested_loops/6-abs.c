e5b9c7f3a6a74192637ca37ae34b81baec50ec30
fedoua <fedoualaasri91@gmail.com>
2023-04-25 17:52:47 -0700

    mycommit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of integr
+ * @i: input number as an integr
+ * return: absolute value
+*/
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
