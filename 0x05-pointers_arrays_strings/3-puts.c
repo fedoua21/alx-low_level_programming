5d24188159c217f9d940aaba60254f19e7ef135a
fedoua <fedoualaasri91@gmail.com>
2023-05-03 16:37:19 -0700

    mycommit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - print a string followed by a new line
+ * @str: string parametre to print
+ * Return: nothing
+*/ 
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
