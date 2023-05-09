7248d6e3f98cc8209987476f4fafd870697d8feb
fedoua <fedoualaasri91@gmail.com>
2023-05-09 03:29:07 -0700

    mycommit

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - write the caracter c to stdout
+ * @c: the caracter to print
+ * Return: on success
+ * On error, -1 is returned
+ */
+int _putchar(char c)
+{
+	return (write(1, @c, 1));
+}
